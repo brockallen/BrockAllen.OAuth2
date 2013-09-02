55998f25b99f37b569024df119e52350a5244f8d
Tom Vandewinckele <vandewinckeletom@hotmail.com>
2013-09-02 16:03:40 +0200

    Fix LinkedIn claims, add linkedIn locality claim support

--- BrockAllen.OAuth2/LinkedInProvider.cs
+++ BrockAllen.OAuth2/LinkedInProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -14,12 +15,12 @@ namespace BrockAllen.OAuth2
             : base(ProviderType.LinkedIn,
                 "https://www.linkedin.com/uas/oauth2/authorization",
                 "https://www.linkedin.com/uas/oauth2/accessToken",
-                "https://api.linkedin.com/v1/people/~",
+                "https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address,picture-url,formatted-name,location:(country:(code)),public-profile-url)",
                 clientID, clientSecret, "oauth2_access_token", new NameValueCollection(){ { "format", "json" } })
         {
             if (scope == null)
             {
-                Scope = "r_basicprofile r_emailaddress";
+                Scope = "r_fullprofile r_emailaddress";
             }
             else
             {
@@ -27,22 +28,50 @@ namespace BrockAllen.OAuth2
             }
         }
 
+        protected override IEnumerable<Claim> GetClaimsFromProfile(Dictionary<string, object> profile)
+        {
+            var claims = base.GetClaimsFromProfile(profile).ToList();
+            var localityClaim = claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.Locality));
+
+            var location = Newtonsoft.Json.JsonConvert.DeserializeObject<LinkedInLocation>(localityClaim.Value);
+
+            claims.Remove(localityClaim);
+
+            localityClaim = new Claim(localityClaim.Type, location.Country.Code);
+            
+            claims.Add(localityClaim);
+
+            return claims;
+        }
+
         static Dictionary<string, string> supportedClaimTypes = new Dictionary<string, string>();
         static LinkedInProvider()
         {
             supportedClaimTypes.Add("id", ClaimTypes.NameIdentifier);
-            supportedClaimTypes.Add("formatted-name", ClaimTypes.Name);
-            supportedClaimTypes.Add("email", ClaimTypes.Email);
-            supportedClaimTypes.Add("first-name", ClaimTypes.GivenName);
-            supportedClaimTypes.Add("last-name", ClaimTypes.Surname);
-            supportedClaimTypes.Add("public-profile-url", ClaimTypes.Webpage);
-            supportedClaimTypes.Add("location:(country:(code))", ClaimTypes.Locality);
-            supportedClaimTypes.Add("picture-url", ClaimTypes.UserData);
+            supportedClaimTypes.Add("formattedName", ClaimTypes.Name);
+            supportedClaimTypes.Add("emailAddress", ClaimTypes.Email);
+            supportedClaimTypes.Add("firstName", ClaimTypes.GivenName);
+            supportedClaimTypes.Add("lastName", ClaimTypes.Surname);
+            supportedClaimTypes.Add("publicProfileUrl", ClaimTypes.Webpage);
+            supportedClaimTypes.Add("location", ClaimTypes.Locality);
+            supportedClaimTypes.Add("pictureUrl", ClaimTypes.UserData);
         }
         
         internal override Dictionary<string, string> SupportedClaimTypes
         {
             get { return supportedClaimTypes; }
+        }
+        
+        private class LinkedInLocation
+        {
+            [JsonProperty("country")]
+            public LinkedInCountry Country { get; set; }
+        }
+
+        private class LinkedInCountry
+        {
+            [JsonProperty("code")]
+            public string Code { get; set; }
         }
     }
 }
